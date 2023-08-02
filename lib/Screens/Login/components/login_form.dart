4cd374e797b9abfad77723c77d9f8a512d9fa15f
EdsonCalixtoo <juninho.caxto@gmail.com>
2023-08-02 20:44:54 -0300

    tela de login inclusa sem Backend

---
+++ lib/Screens/Login/components/login_form.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_feed_ui_redesign/Screens/feed_screen.dart';
+
+import '../../../components/already_have_an_account_acheck.dart';
+import '../../../constants.dart';
+import '../../Signup/signup_screen.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kSecondColor,
+            onSaved: (email) {},
+            decoration: InputDecoration(
+              hintText: "Exemplo@gmail.com",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kSecondColor,
+              decoration: InputDecoration(
+                hintText: "Senha",
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          Hero(
+            tag: "login_btn",
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FeedScreen()),
+                );
+              },
+              child: Text(
+                "Login".toUpperCase(),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SignUpScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Colors.pinkAccent;
+const kPrimaryLightColor = Colors.blueAccent;
+const kSecondColor = Colors.white;
+
+const double defaultPadding = 16.0;
